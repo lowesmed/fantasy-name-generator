7fc088f97a9643f7c3a40ea6998315f60aab1ad4
Lowe Smed <ls222cg@student.lnu.se>
2026-09-04 13:05:33 +0200

    Make form its own component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-
+import Form from "@/components/form";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black gap-8">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black gap-6">
         <div className="flex flex-col items-center gap-6 text-center sm:text-center">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Fantasy Name Ganerator
@@ -12,24 +12,7 @@ export default function Home() {
             Please enter your first name
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="#"
-            rel="noopener noreferrer"
-          >
-            Your name
-          </a>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="#"
-            rel="noopener noreferrer"
-          >
-            Generate now
-          </a>
-        </div>
+        <Form />
       </main>
     </div>
   );
---
+++ components/form.tsx
@@ -0,0 +1,23 @@
+export default function Form() {
+  return (
+    <div className="flex flex-col gap-4">
+        <div className="flex flex-col text-base font-medium sm:flex-row">
+          <input
+            type="text"
+            name=""
+            placeholder="Your name"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px] text-center outline-0"
+             />
+        </div>
+        <div className="flex flex-col text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="#"
+            rel="noopener noreferrer"
+          >
+            Generate now
+          </a>
+        </div>
+</div>
+  );
+}
