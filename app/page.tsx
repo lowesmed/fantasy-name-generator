e55796f1622eae85677251a78f761f616e9ad511
Lowe Smed <ls222cg@student.lnu.se>
2026-09-07 17:03:14 +0200

    Update styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import NameForm from "@/components/name-form";
+// import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black gap-6">
+    <div className="flex flex-col flex-1 items-center justify-center font-sans bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-black gap-6">
         <div className="flex flex-col items-center gap-6 text-center sm:text-center">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-zinc-50">
             Fantasy Name Ganerator
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="max-w-md text-lg leading-8 text-zinc-400">
             Create a legendary title for your fantasy character inspired by your
             own first name.
           </p>
--- components/name-form.tsx
+++ components/name-form.tsx
@@ -15,18 +15,18 @@ export default function NameForm() {
           name="name"
           id="name"
           placeholder="Your name"
-          className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-39.5 text-center outline-0"
+          className="flex h-12 w-full items-center justify-center rounded-full border border-solid px-5 transition-colors hover:bg-black/4 border-white/[.145] md:w-39.5 text-center outline-0"
         />
         <button
-          className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5"
+          className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-purple-800 px-5 text-foreground transition-colors hover:bg-purple-900 md:w-39.5"
           type="submit"
         >
           {isLoading ? "Making magic..." : "Generate"}
         </button>
         {state && (
-          <div className="flex flex-col gap-2 text-base text-center border-2 p-8 mt-6 border-purple-900 rounded-2xl">
-            <p className="font-medium">Welome</p>
-            <p className="text-2xl font-bold text-purple-800">{state}</p>
+          <div className="flex flex-col gap-2 text-base text-center border-2 p-8 mt-6 border-purple-800 rounded-2xl">
+            <p className="font-medium">Hello</p>
+            <p className="text-2xl font-bold text-purple-600">{state}</p>
             <p>Now of to battle!</p>
           </div>
         )}
