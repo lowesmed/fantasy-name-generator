af7552d6f9c3a946ef4368cdc8fb90e1f4fdf6bd
Lowe Smed <ls222cg@student.lnu.se>
2026-09-07 12:53:27 +0200

    Add suffix & prefix to naming modell

--- lib/generate-fantasy-name.ts
+++ lib/generate-fantasy-name.ts
@@ -1,10 +1,18 @@
 /**
- * Takes a regular name and creates a fantasy version of it.
+ * Takes a regular name and creates a fantasy version of it. By using the first three letters 
  * 
  * @param firstName - The name to make fantasy style 
  * @returns {string} - Fantasy name
  */
 export function generateFantasyName(firstName: string) : string {
 
-  return "A fantasy name of: " + firstName;
+  const suffix = ["dor", "thar", "wyn", "gorn", "mar", "ion", "riel"];
+
+  const prefix = ["Ar", "Bel", "Thal", "Mor", "Kael", "Eld", "Shad", "Val"];
+
+  const firstLetters = firstName.substring(0,2);
+
+  console.log(firstLetters)
+
+  return "A fantasy name of: " + firstLetters + suffix[Math.floor(Math.random() * suffix.length)];
 }
