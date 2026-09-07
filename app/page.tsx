588e7af32db08e1428a061926674d7fc1e8c6297
Lowe Smed <ls222cg@student.lnu.se>
2026-09-07 17:11:58 +0200

    Add logo image

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import NameForm from "@/components/name-form";
-// import Image from 'next/image'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-black gap-6">
         <div className="flex flex-col items-center gap-6 text-center sm:text-center">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-zinc-50">
-            Fantasy Name Ganerator
-          </h1>
+          <Image 
+            src="/fantasy-name-generator-logo.jpg"
+            width={360}
+            height={360}
+            alt="Fantasy Name Generator"
+          />
           <p className="max-w-md text-lg leading-8 text-zinc-400">
             Create a legendary title for your fantasy character inspired by your
             own first name.
